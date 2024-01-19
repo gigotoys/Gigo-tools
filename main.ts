28143ce0c69c31619e168d9cb28ad243acdcee46
gigotoys <40448462+gigotoys@users.noreply.github.com>
2024-01-19 17:43:33 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -545,13 +545,15 @@ export function ColorSensorinit(): void {
                 ReadGreenColor = [TCS_RED, TCS_GREEN, TCS_BLUE]
                 break;
             case 3:
-                ReadBlueColor = [TCS_RED, TCS_GREEN, TCS_BLUE +30]
+                TCS_BLUE +=30;
+                ReadBlueColor = [TCS_RED, TCS_GREEN, TCS_BLUE]
                 break;
             case 4:
                 ReadYellowColor = [TCS_RED, TCS_GREEN, TCS_BLUE]
                 break;
             case 5:
-                ReadPurpleColor = [TCS_RED, TCS_GREEN, TCS_BLUE +30]
+                TCS_BLUE +=30;
+                ReadPurpleColor = [TCS_RED, TCS_GREEN, TCS_BLUE]
                 break;
             case 6:
                 ReadCustom1Color = [TCS_RED, TCS_GREEN, TCS_BLUE]
